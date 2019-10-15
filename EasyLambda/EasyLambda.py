fdb87474cd8545ac9d62d4ce0e84081e69be30d3
Damian Sloane <dddaaammmooo@hotmail.com>
2019-10-16 10:48:54 +1100

    Interim commit, work in progress...

--- EasyLambda/EasyLambda.py
+++ EasyLambda/EasyLambda.py
@@ -24,12 +24,10 @@ class EasyLambda:
             try:
                 log_level = self.get_aws_event_parameter('log_level')
                 Log.debug('Requested Log Level: {log_level}'.format(log_level=log_level))
-                log_level = int(log_level)
-                Log.debug('Integer Log Level: {log_level}'.format(log_level=log_level))
-                Log.set_level(log_level)
+                Log.set_level(int(log_level))
             except Exception as log_exception:
-                Log.error('An unexpected error occurred while attempting to set desired logging level.')
-                Log.error(log_exception)
+                print('An unexpected error occurred while attempting to set desired logging level.')
+                raise Exception(log_exception)
                 pass
 
         try:
