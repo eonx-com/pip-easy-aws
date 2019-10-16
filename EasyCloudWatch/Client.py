2bcae88ef1c1b7f988f46ab2c891afac8496c0b5
Damian Sloane <dddaaammmooo@hotmail.com>
2019-10-16 11:44:37 +1100

    Interim commit, work in progress...

--- EasyCloudWatch/Client.py
+++ EasyCloudWatch/Client.py
@@ -8,6 +8,34 @@ from EasyLog.Log import Log
 
 
 class Client:
+    # Allow metrics
+    UNIT_SECONDS = 'Seconds'
+    UNIT_MICROSECONDS = 'Microseconds'
+    UNIT_MILLISECONDS = 'Milliseconds'
+    UNIT_BYTES = 'Bytes'
+    UNIT_KILOBYTES = 'Kilobytes'
+    UNIT_MEGABYTES = 'Megabytes'
+    UNIT_GIGABYTES = 'Gigabytes'
+    UNIT_TERABYTES = 'Terabytes'
+    UNIT_BITS = 'Bits'
+    UNIT_KILOBITS = 'Kilobits'
+    UNIT_MEGABITS = 'Megabits'
+    UNIT_GIGABITS = 'Gigabits'
+    UNIT_TERABITS = 'Terabits'
+    UNIT_PERCENT = 'Percent'
+    UNIT_COUNT = 'Count'
+    UNIT_BYTES_PER_SECOND = 'Bytes / Second'
+    UNIT_KILOBYTES_PER_SECOND = 'Kilobytes / Second'
+    UNIT_MEGABYTES_PER_SECOND = 'Megabytes / Second'
+    UNIT_GIGABYTES_PER_SECOND = 'Gigabytes / Second'
+    UNIT_TERABYTES_PER_SECOND = 'Terabytes / Second'
+    UNIT_BITS_PER_SECOND = 'Bits / Second'
+    UNIT_KILOBITS_PER_SECOND = 'Kilobits / Second'
+    UNIT_MEGABITS_PER_SECOND = 'Megabits / Second'
+    UNIT_GIGABITS_PER_SECOND = 'Gigabits / Second'
+    UNIT_TERABITS_PER_SECOND = 'Terabits / Second'
+    UNIT_COUNT_PER_SECOND = 'Count / Second'
+
     # Cache for CloudWatch client
     __client__ = None
 
@@ -18,7 +46,7 @@ class Client:
         """
         # If we haven't gotten a client yet- create one now and cache it for future calls
         if Client.__client__ is None:
-            Log.trace('Instantiating AWS CloudWatch client...')
+            Log.trace('Instantiating AWS CloudWatch Client...')
             Client.__client__ = boto3.session.Session().client('cloudwatch')
 
         # Return the cached client
@@ -59,5 +87,5 @@ class Client:
         Log.trace('Incrementing AWS CloudWatch counter: {metric_name}...'.format(metric_name=metric_name))
         Client.get_cloudwatch_client().put_metric_data(
             Namespace=os.environ.get('AWS_LAMBDA_FUNCTION_NAME'),
-            MetricData=[{'MetricName': metric_name, 'Unit': 'Count', 'Value': 1.0}]
+            MetricData=[{'MetricName': metric_name, 'Unit': Client.UNIT_COUNT, 'Value': 1.0}]
         )
