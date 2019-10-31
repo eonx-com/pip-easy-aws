4804487fada73d9b3b7afef87455acce8e17729f
Damian Sloane <damian@capacitorlabs.com.au>
2019-10-31 12:03:33 +1100

    Interim commit, work in progress...

--- setup.sh
+++ setup.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
 cd "$(dirname "$0")"
-npm install serverless-python-requirements --save-dev
-npm install serverless-deployment-bucket --save-dev
 rm -rf ./venv
 python3 -m venv ./venv
 source ./venv/bin/activate
