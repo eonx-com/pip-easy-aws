d6f557b61a13c9530704ea86e4835a33092e3e20
Damian Sloane <dddaaammmooo@hotmail.com>
2019-11-04 23:52:02 +1100

    WIP

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='EasyLambda',
-    version='2.0.0',
+    version='3.0.0',
     description='Easy Lambda Function Library',
     url='git@github.com:loyaltycorp/easy_lambda',
     author='Damian Sloane',
